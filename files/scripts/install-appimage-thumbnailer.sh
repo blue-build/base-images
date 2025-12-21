4d19c1e41336f60569a136358929a5186e8039f4
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-12-21 18:34:52 +0100

    Add `appimage-thumbnailer` and its dependency `7zip` (#16)
    Useful for showing thumbnails for AppImages.
    
    Only available for `x86_64`, so I'm only installing it there.
    
    `7zip` is a dependency to it, so I'm also installing it.

---
+++ files/scripts/install-appimage-thumbnailer.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -eu
+
+if [ "$OS_ARCH" = "x86_64" ]; then
+  github_json=$(curl -fLsS --retry 5 -H 'Accept: application/vnd.github+json' https://api.github.com/repos/kem-a/appimage-thumbnailer/releases/latest)
+  release_assets=$(echo "$github_json" | jq -cr '.assets | map({(.name | tostring): .browser_download_url}) | add')
+  rpm_url=$(echo "$release_assets" | jq -cr '.[keys_unsorted[] | select(test("appimage-thumbnailer.*x86_64\\.rpm$"))]')
+  curl -fLsS --retry 5 -o "/tmp/appimage-thumbnailer.rpm" "$rpm_url"
+  dnf -y install "/tmp/appimage-thumbnailer.rpm" 7zip glib2 gdk-pixbuf2 librsvg2 cairo
+  rm -f "/tmp/appimage-thumbnailer.rpm"
+else
+  echo "AppImage thumbnailer is not available for 'aarch64' and other architectures yet"
+fi
