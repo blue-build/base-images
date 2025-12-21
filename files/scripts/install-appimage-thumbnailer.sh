8f2c1c3d678a82cd8925f557fa3a776081f6cae6
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-12-21 14:56:37 +0100

    Add `appimage-thumbnailer` and its dependency `7zip`

---
+++ files/scripts/install-appimage-thumbnailer.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -eu
+
+if [ "$OS_ARCH" = "x86_64" ]; then
+  release_assets=$(curl -fLsS --retry 5 -H 'Accept: application/vnd.github+json' \
+  https://api.github.com/repos/kem-a/appimage-thumbnailer/releases/latest \
+  | jq -cr '.assets | map({(.name | tostring): .browser_download_url}) | add')
+  rpm_url=$(jq -cr '.[keys_unsorted[] | select(test("appimage-thumbnailer.*x86_64\\.rpm$"))]' <<< "${release_assets}")
+  curl -fLsS --retry 5 -o "/tmp/appimage-thumbnailer.rpm" "$rpm_url"
+  dnf5 -y install "/tmp/appimage-thumbnailer.rpm" 7zip
+else
+  echo "AppImage thumbnailer is not available for 'aarch64' and other architectures yet"
+fi
