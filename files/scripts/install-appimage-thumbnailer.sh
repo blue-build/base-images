c1fdcad7574c653f2272b3ccbc904fa27e220035
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-12-21 15:13:46 +0100

    Make it POSIX again

--- files/scripts/install-appimage-thumbnailer.sh
+++ files/scripts/install-appimage-thumbnailer.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-set -euo pipefail
+set -eu
 
 if [ "$OS_ARCH" = "x86_64" ]; then
   release_assets=$(curl -fLsS --retry 5 -H 'Accept: application/vnd.github+json' \
   https://api.github.com/repos/kem-a/appimage-thumbnailer/releases/latest \
   | jq -cr '.assets | map({(.name | tostring): .browser_download_url}) | add')
-  rpm_url=$(jq -cr '.[keys_unsorted[] | select(test("appimage-thumbnailer.*x86_64\\.rpm$"))]' <<< "${release_assets}")
+  rpm_url=$(echo "$release_assets" | jq -cr '.[keys_unsorted[] | select(test("appimage-thumbnailer.*x86_64\\.rpm$"))]')
   curl -fLsS --retry 5 -o "/tmp/appimage-thumbnailer.rpm" "$rpm_url"
   dnf5 -y install "/tmp/appimage-thumbnailer.rpm" 7zip
 else
